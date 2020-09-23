e1ffbb9991b23c205ea3cc2d81a81990243aa7d0
Vibhu2 <vibhubhatnagar@outlook.com>
2020-09-23 21:39:32 +0530

    commented removal lines

--- ExclaimerScriptDeploymet.ps1
+++ ExclaimerScriptDeploymet.ps1
@@ -9,8 +9,8 @@ $path=Test-Path "C:\itconnexx-a"
 if (!(test-path $path) ){New-Item C:\itconnexx-a -ItemType Directory}
 
 Start-Process -FilePath 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi' -PassThru '/quiet -m'
-Start-Sleep -Seconds 60
-Remove-Item 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi'
-Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.xml'
-Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.ps1'
+#Start-Sleep -Seconds 60
+#Remove-Item 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi'
+#Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.xml'
+#Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.ps1'
 
