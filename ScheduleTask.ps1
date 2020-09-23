b840cd4c82c19bc4d8483e57d7a1f3242ac7a5ed
Vibhu2 <vibhubhatnagar@outlook.com>
2020-09-23 22:27:39 +0530

    Update ScheduleTask.ps1

--- ScheduleTask.ps1
+++ ScheduleTask.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference= 'silentlycontinue'
 if (!(test-path $path) )
 {New-Item C:\itconnexx-a -ItemType Directory}
 Start-BitsTransfer -Source https://outlookclient.exclaimer.net/csua/Exclaimer.CloudSignatureUpdateAgent.Install.msi -Destination "$Installpath" -Verbose
-Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/master/ExclaimerScriptDeploymet.ps1 -Destination "$Installpath" -Verbose
+Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/temp/ExclaimerScriptDeploymet.ps1 -Destination "$Installpath" -Verbose
 Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/master/ExclaimerScriptDeploymet.xml -Destination "$Installpath" -Verbose
 
 #Export-ScheduledTask -TaskName "Exclaimer Client Deployment" "\TASK-PATH-TASKSCHEDULER\" | Out-File C:\itconnexx-a\ExclaimerScriptDeploymet.xml
