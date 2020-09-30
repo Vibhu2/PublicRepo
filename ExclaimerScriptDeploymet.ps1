462838a47610acd4fd93ed6909a5ed637290adb5
Vibhu2 <vibhubhatnagar@outlook.com>
2020-09-30 18:40:49 +0530

    Update ExclaimerScriptDeploymet.ps1

--- ExclaimerScriptDeploymet.ps1
+++ ExclaimerScriptDeploymet.ps1
@@ -1,4 +1,9 @@
-﻿
+﻿$username=$env:USERNAME
+
+$Result=Test-Path -Path "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Exclaimer\Cloud Signature Update Agent.lnk"
+
+if($Result=
+
 Set-ExecutionPolicy Bypass -scope Process -Force
 Get-Process *installer* | Stop-Process -Force
 $ErrorActionPreference= 'silentlycontinue'
