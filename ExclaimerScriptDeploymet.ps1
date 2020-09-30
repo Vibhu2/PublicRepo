10230f3c7d444f0e9c5fa613e603ee1d14bfe0ce
Vibhu2 <vibhubhatnagar@outlook.com>
2020-09-30 20:20:52 +0530

    updated script

--- ExclaimerScriptDeploymet.ps1
+++ ExclaimerScriptDeploymet.ps1
@@ -2,20 +2,20 @@
 
 $Result=Test-Path -Path "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Exclaimer\Cloud Signature Update Agent.lnk"
 
-if($Result=
+if (!(test-path $Result) ){
 
-Set-ExecutionPolicy Bypass -scope Process -Force
-Get-Process *installer* | Stop-Process -Force
-$ErrorActionPreference= 'silentlycontinue'
+    Set-ExecutionPolicy Bypass -scope Process -Force
+    Get-Process *installer* | Stop-Process -Force
+    $ErrorActionPreference= 'silentlycontinue'
 
-$Installpath= "C:\itconnexx-a"
-$path=Test-Path "C:\itconnexx-a"
+    $Installpath= "C:\itconnexx-a"
+    $path=Test-Path "C:\itconnexx-a"
 
-if (!(test-path $path) ){New-Item C:\itconnexx-a -ItemType Directory}
+    if (!(test-path $path) ){New-Item C:\itconnexx-a -ItemType Directory}
 
-Start-Process -FilePath 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi' -PassThru '/quiet -m'
-#Start-Sleep -Seconds 60
-#Remove-Item 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi'
-#Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.xml'
-#Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.ps1'
-
+    Start-Process -FilePath 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi' -PassThru '/quiet -m'
+    #Start-Sleep -Seconds 60
+    #Remove-Item 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi'
+    #Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.xml'
+    #Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.ps1'
+}
--- ImprovedDeployment.ps1
+++ ImprovedDeployment.ps1
@@ -2,9 +2,10 @@
 $Installpath= "C:\itconnexx-a"
 $path=Test-Path "C:\itconnexx-a"
 $ErrorActionPreference= 'silentlycontinue'
+$UniversalStartup='C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp'
 
 if (!(test-path $path) )
 {New-Item C:\itconnexx-a -ItemType Directory}
 Start-BitsTransfer -Source https://outlookclient.exclaimer.net/csua/Exclaimer.CloudSignatureUpdateAgent.Install.msi -Destination "$Installpath" -Verbose
 Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/temp/ExclaimerScriptDeploymet.ps1 -Destination "$Installpath" -Verbose
-Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/master/ExclaimerScriptDeploymet.xml -Destination "$Installpath" -Verbose
+Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/master/install.bat -Destination "$UniversalStartup" -Verbose
