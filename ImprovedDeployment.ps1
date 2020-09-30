aad37c589f25f485b13a593c5817837e4013dee3
Vibhu2 <vibhubhatnagar@outlook.com>
2020-09-30 20:23:11 +0530

    Update ImprovedDeployment.ps1

--- ImprovedDeployment.ps1
+++ ImprovedDeployment.ps1
@@ -1,4 +1,6 @@
-﻿Set-ExecutionPolicy Bypass -scope Process -Force
+﻿# This part of the script is just to prepare the enviornment
+
+Set-ExecutionPolicy Bypass -scope Process -Force
 $Installpath= "C:\itconnexx-a"
 $path=Test-Path "C:\itconnexx-a"
 $ErrorActionPreference= 'silentlycontinue'
@@ -9,3 +11,27 @@ if (!(test-path $path) )
 Start-BitsTransfer -Source https://outlookclient.exclaimer.net/csua/Exclaimer.CloudSignatureUpdateAgent.Install.msi -Destination "$Installpath" -Verbose
 Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/temp/ExclaimerScriptDeploymet.ps1 -Destination "$Installpath" -Verbose
 Start-BitsTransfer -Source https://raw.githubusercontent.com/Vibhu2/PublicRepo/master/install.bat -Destination "$UniversalStartup" -Verbose
+
+#this part of the script checks for the installed software and if not installed for the user it deploys it.
+
+$username=$env:USERNAME
+
+$Result=Test-Path -Path "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Exclaimer\Cloud Signature Update Agent.lnk"
+
+if (!(test-path $Result) ){
+
+    Set-ExecutionPolicy Bypass -scope Process -Force
+    Get-Process *installer* | Stop-Process -Force
+    $ErrorActionPreference= 'silentlycontinue'
+
+    $Installpath= "C:\itconnexx-a"
+    $path=Test-Path "C:\itconnexx-a"
+
+    if (!(test-path $path) ){New-Item C:\itconnexx-a -ItemType Directory}
+
+    Start-Process -FilePath 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi' -PassThru '/quiet -m'
+    #Start-Sleep -Seconds 60
+    #Remove-Item 'C:\itconnexx-a\Exclaimer.CloudSignatureUpdateAgent.Install.msi'
+    #Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.xml'
+    #Remove-Item 'C:\itconnexx-a\ExclaimerScriptDeploymet.ps1'
+}
